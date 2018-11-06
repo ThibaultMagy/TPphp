6f61fd40859660d6d343c49d1df43814523d7b34
Thibault Magy <magy.thibault@gmail.com>
2018-11-06 15:26:58 +0100

    Ajout des éléments de "Ajouter un parcours"

--- covoit-master/include/pages/ajouterParcours.inc.php
+++ covoit-master/include/pages/ajouterParcours.inc.php
@@ -1,1 +1,25 @@
 <?php
+include('classes/Ville.class.php');
+?>
+<h1>Ajouter un parcours</h1>
+
+<FORM>
+  Ville1<SELECT>
+    <?php
+    while($row=mysqli_fetch_array($res,MY_SQL_ASSOC)){
+      echo '<OPTION>';
+      echo $row['vil_nom'];
+    }
+    ?>
+  </SELECT>
+
+  Ville2<SELECT>
+    <?php
+    ?>
+  </SELECT>
+
+  Nombre de kilomètre(s)<input placeholder=" ex: 250"></input>
+
+  <button type="button">valider</button>
+
+</FORM>

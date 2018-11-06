0e5089b681a0e4bd1290f1903cd2b7f1e80a5d38
Thibault Magy <magy.thibault@gmail.com>
2018-11-06 16:23:02 +0100

    Ajout de code dans "ajouterPersonne" et modifiaction css

--- covoit-master/include/pages/ajouterPersonne.inc.php
+++ covoit-master/include/pages/ajouterPersonne.inc.php
@@ -1,3 +1,27 @@
+<?php include('classes/Personne.class.php'); ?>
+<h1>Ajouter une personne</h1>
 
-				<h1>Ajouter une personne</h1>
-	
+<FORM>
+  <div id="gauche" class="gauche">
+    <div id="texteGauche" class="gauche">
+      <p>Nom :           </p><input placeholder=" ex : Durand" />
+      <br />
+      <p>Téléphone :     </p><input placeholder=" ex : 0555434355" />
+      <br />
+      <p>Login :         </p><input placeholder=" ex : jojo" />
+  </div>
+
+  <div id="droite" class="droite">
+    <p>Prénom :        </p><input placeholder=" ex : Joe" />
+    <br />
+    <p>Mail :          </p><input placeholder=" ex jojo@joemail.com" />
+    <br />
+    <p>Mot de passe :  </p><input />
+  </div>
+
+  <div>
+    <input type="radio" name="categorie" value="Etudiant" id="etu" class="rdButton"/> <label for="etu">Etudiant</label><br />
+    <input type="radio" name="categorie" value="Personnel" id="perso" class="rdButton"/> <label for="perso">Personnel</label><br />
+    <input type="submit" Value="Valider" class="subButton" />
+  </div>
+</FORM>

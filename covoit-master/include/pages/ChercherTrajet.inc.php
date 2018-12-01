94cd866a39c2fec2e9a826798fc17293dfc53ef4
Thibault Magy <magy.thibault@gmail.com>
2018-12-01 15:40:00 +0100

    Structure Cjercjer trajet

--- covoit-master/include/pages/ChercherTrajet.inc.php
+++ covoit-master/include/pages/ChercherTrajet.inc.php
@@ -1,1 +1,27 @@
 <h1>Rechercher un trajet</h1>
+
+<form>
+  
+  <div class="formBloc">
+    <div class="labelinput">
+      <label>Ville de départ : </label>
+    </div>
+    <div class="labelinput">
+      <label>Date de départ : </label>
+    </div>
+    <div class="labelinput">
+      <label>A partir de : </label>
+    </div>
+  </div>
+
+  <div class="formBloc">
+    <div class="labelinput">
+      <label>Ville d'arrivée : </label>
+    </div>
+    <div class="labelinput">
+      <label>Précision : </label>
+    </div>
+    <input type="submit" class="subButton2" name="bouton" value="Valider">
+  </div>
+
+</form>

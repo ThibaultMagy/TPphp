310d14ec77c240eb4cb1135ed46c574ba5a87450
Thibault Magy <magy.thibault@gmail.com>
2018-12-01 15:45:20 +0100

    Structure selects de chercherTrajet

--- covoit-master/include/pages/ChercherTrajet.inc.php
+++ covoit-master/include/pages/ChercherTrajet.inc.php
@@ -1,26 +1,59 @@
 <h1>Rechercher un trajet</h1>
 
 <form>
-  
-  <div class="formBloc">
-    <div class="labelinput">
-      <label>Ville de départ : </label>
-    </div>
-    <div class="labelinput">
-      <label>Date de départ : </label>
-    </div>
-    <div class="labelinput">
-      <label>A partir de : </label>
-    </div>
-  </div>
 
   <div class="formBloc">
     <div class="labelinput">
-      <label>Ville d'arrivée : </label>
+      <label>Ville de départ : </label>
+      <select class="" name="" required>
+        <option value="0"> Choisissez</option>
+        <?php foreach ($nomInstance as $objet){ ?>
+          <option value="<?php echo $objet->getObjet() ?>"><?php echo $objetManager->getObjetId($objet->getObjet());?></option>
+        <?php } ?>
+      </select>
     </div>
+
+    <div class="labelinput">
+      <label>Date de départ : </label>
+    </div>
+
+    <div class="labelinput">
+      <label>A partir de : </label>
+      <select class="" name="" required>
+        <option value="0"> Choisissez</option>
+        <?php foreach ($nomInstance as $objet){ ?>
+          <option value="<?php echo $objet->getObjet() ?>"><?php echo $objetManager->getObjetId($objet->getObjet());?></option>
+        <?php } ?>
+      </select>
+    </div>
+
+  </div>
+
+
+
+
+  <div class="formBloc">
+
+    <div class="labelinput">
+      <label>Ville d'arrivée : </label>
+      <select class="" name="" required>
+        <option value="0"> Choisissez</option>
+        <?php foreach ($nomInstance as $objet){ ?>
+          <option value="<?php echo $objet->getObjet() ?>"><?php echo $objetManager->getObjetId($objet->getObjet());?></option>
+        <?php } ?>
+      </select>
+    </div>
+
     <div class="labelinput">
       <label>Précision : </label>
+      <select class="" name="" required>
+        <option value="0"> Choisissez</option>
+        <?php foreach ($nomInstance as $objet){ ?>
+          <option value="<?php echo $objet->getObjet() ?>"><?php echo $objetManager->getObjetId($objet->getObjet());?></option>
+        <?php } ?>
+      </select>
     </div>
+
     <input type="submit" class="subButton2" name="bouton" value="Valider">
   </div>
 

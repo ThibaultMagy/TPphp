c71d7d5652d8aaa00038f6c6458868ea954e6e16
Thibault Magy <magy.thibault@gmail.com>
2018-12-02 14:45:57 +0100

    Correction structure ajouter parcours 1

--- covoit-master/include/pages/ajouterParcours.inc.php
+++ covoit-master/include/pages/ajouterParcours.inc.php
@@ -10,25 +10,31 @@ $tabVille = $villeManager->getAllVilles();
 
  <?php if (empty($_POST["vil_num1"]) && empty($_POST["vil_num2"]) && empty($_POST["par_km"])){ ?>
    <form class="" action="#" method="post">
-     <p>
-     <label> Ville 1 : </label>
-     <select class="" name="vil_num1" required>
-       <?php foreach ($tabVille as $ville): ?>
-         <option value="<?php echo $ville->getVilNum() ?>"><?php echo $ville->getVilNom()?></option>
-       <?php endforeach; ?>
-       </select>
+     <div class="formBloc">
+       <div class="labelinputinline">
+         <label> Ville 1 : </label>
+         <select class="" name="vil_num1" required>
+           <?php foreach ($tabVille as $ville): ?>
+             <option value="<?php echo $ville->getVilNum() ?>"><?php echo $ville->getVilNom()?></option>
+           <?php endforeach; ?>
+           </select>
+         </div>
 
-       <label> Ville 2 : </label>
-       <select class="" name="vil_num2" required>
-         <?php foreach ($tabVille as $ville): ?>
-           <option value="<?php echo $ville->getVilNum() ?>"><?php echo $ville->getVilNom()?></option>
-         <?php endforeach;?>
-       </select>
-      <label> Nb de km : </label>
-      <input type="number" name="par_km" value="" required>
+         <div class="labelinputinline">
+         <label> Ville 2 : </label>
+           <select class="" name="vil_num2" required>
+             <?php foreach ($tabVille as $ville): ?>
+               <option value="<?php echo $ville->getVilNum() ?>"><?php echo $ville->getVilNom()?></option>
+             <?php endforeach;?>
+           </select>
+        </div>
+        <div class="labelinputinline">
+          <label> Nb de km : </label>
+          <input type="number" name="par_km" value="" required>
+        </div>
+      </div>
 
-    </p>
-    <input  class="subButton" type="submit" value="Valider">
+    <input  class="subButton2" type="submit" value="Valider" />
    </form>
 
 <?php  } else {
